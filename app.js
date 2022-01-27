f71a0c9066453796d20eb5e28a4eab2a42a92a6d
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-01-27 13:32:23 +0200

    add function for toggle modal in js

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // start app project
 console.log('start projects');
+
+
+// *Toggle Add form
+const buttonShowForm = document.getElementById('button-show-form');
+const buttonCloseModal = document.getElementById('close-modal');
+const modalContainer = document.getElementById('modal-container');
+const outerModal = document.getElementById('outer-modal');
+
+// ? add Event Listener to open modal
+buttonShowForm.addEventListener('click', openModal);
+
+// ? add the function for open Modal
+function openModal() {
+  modalContainer.classList.remove('modal-hide');
+  modalContainer.classList.add('modal-show');
+}
+
+// ? add Event Listener to close modal
+buttonCloseModal.addEventListener('click', closeModal);
+
+// ? add Event Listener to close modal for outer Modal
+outerModal.addEventListener('click', closeModal);
+
+
+// ? add the function for close Modal
+function closeModal() {
+  modalContainer.classList.add('modal-hide');
+  modalContainer.classList.remove('modal-show');
+}
+
+// * *** ---------------- end toggle modal  --------------  ***
